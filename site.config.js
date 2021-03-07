16b623b6da4df49368a2b95f1813018ed7c12386
Skyler <51185496+skylershu@users.noreply.github.com>
2021-03-07 18:54:57 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e1f30531e7a44f20b43711cfd3d30083',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Skyler Shu',
+  domain: 'beta.skylershu.com',
+  author: 'Skyler Shu',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Skyler ShuのWebsite',
+  socialImageTitle: 'Skyler Shu',
+  socialImageSubtitle: 'Skyler ShuのWebsite',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'SkylerHQ',
+  github: 'skylershu',
+  linkedin: ' ',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
